b770e76746364d67dcd9145ee2b376d4721be5c2
Aalavya <alavya.pandey@gmail.com>
2025-07-04 14:47:00 +0530

    fixed bug

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return <div></div>;
-}
-// *NOTE - We write "use client" on top of the page to make it a client side instead of server side
-// npx convex dev  to run convex locally
