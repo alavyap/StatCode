c20b17da2191deaa31e13cde4e57332a995440eb
Aalavya <alavya.pandey@gmail.com>
2025-07-05 17:08:28 +0530

    intergrated clerk and convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-
+import { SignOutButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="bg-blue h-0.5">StatCode Home Page</div>;
+  return (
+    <div>
+      <SignUpButton />
+      <br />
+      <SignOutButton />
+    </div>
+  );
 }
 // *NOTE - we write "use client" to make the page client side, by default it is on server
